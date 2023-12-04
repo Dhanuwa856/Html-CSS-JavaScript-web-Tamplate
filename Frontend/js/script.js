1b860bf7e56d656f6efd066ed514461821e01336
Dhanushka Rathnayaka <pdhanushka856@gmail.com>
2023-12-04 16:02:32 +0530

    All animation complete

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -104,3 +104,67 @@ function scrollSection() {
 }
 
 const scrollInterval = setInterval(scrollSection, 4000); // Adjust the interval time for scrolling speed
+
+const cardAs = document.querySelectorAll(".card-a");
+const cardBs = document.querySelectorAll(".card-b");
+const cardCs = document.querySelectorAll(".card-c");
+const cardDs = document.querySelectorAll(".card-d");
+const cardXs = document.querySelectorAll(".card-x");
+const cardYs = document.querySelectorAll(".card-y");
+const cardZs = document.querySelectorAll(".card-z");
+const cardPs = document.querySelectorAll(".card-p");
+const cardImgs = document.querySelectorAll(".card-img");
+const notSees = document.querySelectorAll(".not-see");
+
+const options = {
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver(function (entries) {
+  entries.forEach((entry) => {
+    const intersecting = entry.isIntersecting;
+    if (intersecting) {
+      entry.target.classList.add("transform0");
+      entry.target.classList.add("see");
+
+      observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove("transform0");
+      entry.target.classList.remove("see");
+    }
+    // entry.target.style.width = intersecting ? "200px":"50px";
+  });
+}, options);
+
+// observer.observe(primaryBtn);
+
+cardAs.forEach((cardA) => {
+  observer.observe(cardA);
+});
+cardBs.forEach((cardB) => {
+  observer.observe(cardB);
+});
+cardCs.forEach((cardC) => {
+  observer.observe(cardC);
+});
+cardDs.forEach((cardD) => {
+  observer.observe(cardD);
+});
+cardXs.forEach((cardX) => {
+  observer.observe(cardX);
+});
+cardYs.forEach((cardY) => {
+  observer.observe(cardY);
+});
+cardZs.forEach((cardZ) => {
+  observer.observe(cardZ);
+});
+cardPs.forEach((cardP) => {
+  observer.observe(cardP);
+});
+cardImgs.forEach((cardImg) => {
+  observer.observe(cardImg);
+});
+notSees.forEach((notSee) => {
+  observer.observe(notSee);
+});
