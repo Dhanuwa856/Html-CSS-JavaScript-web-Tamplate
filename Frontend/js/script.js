4c291f1134ac640e80c39b529a62719857d91ec0
Dhanushka Rathnayaka <pdhanushka856@gmail.com>
2023-12-04 08:58:36 +0530

    video section complete

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -37,12 +37,50 @@ window.addEventListener("scroll", handleStickyNav);
 // Initial setup
 handleStickyNav();
 
-
+// Re Home Button function
 
 function scrollToElement() {
   // Get the target element
-  const targetElement = document.getElementById('targetElement');
+  const targetElement = document.getElementById("targetElement");
 
   // Scroll to the target element
-  targetElement.scrollIntoView({ behavior: 'smooth' });
+  targetElement.scrollIntoView({ behavior: "smooth" });
+}
+
+// counter Animation
+const counters = document.querySelectorAll(".count");
+const speed = 2000;
+
+counters.forEach((counter) => {
+  const animate = () => {
+    const value = +counter.getAttribute("akhi");
+    const data = +counter.innerText;
+
+    const time = value / speed;
+    if (data < value) {
+      counter.innerText = Math.ceil(data + time);
+      setTimeout(animate, 70);
+    } else {
+      counter.innerText = value;
+    }
+  };
+
+  animate();
+});
+// food menu button function
+
+function toggleClass(clickedButton) {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => button.classList.remove("active-btn"));
+  clickedButton.classList.add("active-btn");
+}
+
+function toggleCategory(category) {
+  // Hide all category content
+  const allCategoryContent = document.querySelectorAll(".category-content");
+  allCategoryContent.forEach((content) => content.classList.remove("active"));
+
+  // Show the selected category content
+  const selectedCategoryContent = document.getElementById(`${category}Content`);
+  selectedCategoryContent.classList.add("active");
 }
